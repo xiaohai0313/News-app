830160f76ad3ac24df5d4454092128abc5e8a1a1
xiaohai0313 <43129938+xiaohai0313@users.noreply.github.com>
2020-05-09 18:05:52 -0700

    backend

---
+++ backend/server.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const url = require('url');
+const cors = require("cors");       //�ǵ�ɾcors
+app.use(cors());
+const Guradian_home_url = "https://content.guardianapis.com/search?api-key=e628090b-804b-44f6-8a69-f33efc0bdf06&section=(sport|business|technology|politics)&show-blocks=all";
+const Guradian_tab_url1 = "https://content.guardianapis.com/";
+const Guradian_tab_url2 = "?api-key=e628090b-804b-44f6-8a69-f33efc0bdf06&show-blocks=all";
+
+const NYTime_home_url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=DIf8BxjjPBV4d7BhKfIQuisngHYrb1ti";
+const NYTime_tab_url1 = "https://api.nytimes.com/svc/topstories/v2/";
+const NYTime_tab_url2 = ".json?api-key=DIf8BxjjPBV4d7BhKfIQuisngHYrb1ti";
+
+
+const autosuggest_url = "https://api.cognitive.microsoft.com/bing/v7.0/suggestions?q=9c85981dd8db40a8b68540a9f9141e1a";
+
+const port = process.env.PORT || 7000;
+//for test
+app.get("/api", function (request, response) {
+    response.send("Hi");
+})
+
+//suggest search
+app.get("/api/Search", function (req, res) {
+    var cur_url = "";
+    var params = url.parse(req.url, true).query;
+    if (params.source == 'G') {
+        cur_url = 'https://content.guardianapis.com/search?q=' + params.keyword + '&api-key=e628090b-804b-44f6-8a69-f33efc0bdf06&show-blocks=all'
+    }
+    else if (params.source == 'N') {
+        cur_url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + params.keyword + '&api-key=DIf8BxjjPBV4d7BhKfIQuisngHYrb1ti'
+    }
+    //console.log("Search")
+    https.get(cur_url, function (response) {
+        var res_text = "";
+        response.on("data", function (data) {
+            res_text += data;
+        });
+        response.on("end", function () {
+            var result = JSON.parse(res_text);
+            console.log(result);
+            return res.send(result);
+        });
+    });
+})
+
+//Detail page
+app.get("/api/Detail", function (req, res) {
+    var cur_url = "";
+    var params = url.parse(req.url, true).query;
+    if (params.source == 'G') {
+        cur_url = "https://content.guardianapis.com/" + params.link + "?api-key=e628090b-804b-44f6-8a69-f33efc0bdf06&show-blocks=all";
+    }
+    else {
+        cur_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=web_url:(%22" + params.link + "%22)&api-key=DIf8BxjjPBV4d7BhKfIQuisngHYrb1ti";
+    }
+    //console.log("Detail")
+
+
+    https.get(cur_url, function (response) {
+        var res_text = "";
+        response.on("data", function (data) {
+            res_text += data;
+        });
+        response.on("end", function () {
+            var result = JSON.parse(res_text);
+            console.log(result);
+            return res.send(result);
+        });
+    });
+})
+
+//general NYTime source
+app.get("/api/NYTime", function (req, res) {
+    var params = url.parse(req.url, true).query;
+    var cur_url = "";
+    if (params['cate'] == "all") {
+        console.log(params['cate'])
+        cur_url = NYTime_home_url;
+    }
+    else {
+        console.log(params['cate'])
+        cur_url = NYTime_tab_url1 + params['cate'] + NYTime_tab_url2;
+    }
+  
+    https.get(cur_url, function (response) {
+        var res_text = "";
+        response.on("data", function (data) {
+            res_text += data;
+        });
+        response.on("end", function () {
+            var result = JSON.parse(res_text);
+
+            return res.send(result);
+        });
+    });
+});
+
+//general Gurdian source
+app.get("/api/Gurdian", function (req, res) {
+    var params = url.parse(req.url, true).query;
+    var cur_url = "";
+    if (params['cate'] == "all") {
+        cur_url = Guradian_home_url;
+    }
+    else {
+        cur_url = Guradian_tab_url1 + params['cate'] + Guradian_tab_url2;
+    }
+    
+    https.get(cur_url, function (response) {
+        var res_text = "";
+        response.on("data", function (data) {
+            res_text += data;
+        });
+        response.on("end", function () {
+            var result = JSON.parse(res_text);
+
+            return res.send(result);
+        });
+    });
+});
+
+app.listen(port, function () {
+    console.log("Start");
+});
